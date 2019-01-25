50e8d844b029bd138c601814b31d83480e5b4ef9
StephenButters13 <45399451+StephenButters13@users.noreply.github.com>
2019-01-25 15:49:51 -0500

    add Lift

---
+++ src/main/java/org/usfirst/frc/team6500/robot/Lift.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team6500.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Lift {
+    Static Spark left, right;
+   private static boolean isReady;
+
+public static void initializeLift () {
+    left = new Spark(Constants.LIFT_MOTOR_LEFT);
+    right = new Spark(Constants.LIFT_MOTOR_RIGHT);
+
+    isReady = true;
+
+}
+}
